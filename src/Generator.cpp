7d0d5a108d9e169d325514362aee003acf2a41d2
Jonathan <jonathan3cats@gmail.com>
2026-08-25 20:38:18 +0100

    Fixed error in price dist

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 //Constructor
-Generator::Generator(double order_rate, uint32_t num_clients, uint32_t min_quantity, uint32_t max_quantity, double reference_price, double mean_price_offset) 
+Generator::Generator(double order_rate, uint32_t num_clients, uint32_t min_quantity, uint32_t max_quantity, double reference_price, double price_stddev) 
     : rng(374), //can change to random seed
       arrival_rate(order_rate / 1e6),
       client_dist(1, num_clients),
       quantity_dist(min_quantity, max_quantity),
-      price_offset_dist(1 / mean_price_offset),
+      price_offset_dist(0, price_stddev),
       type_dist(0.5){
         order_id_tracker = 1;
         ref = reference_price;
